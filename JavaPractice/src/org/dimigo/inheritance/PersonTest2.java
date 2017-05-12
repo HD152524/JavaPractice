cbdc7be83a15cfc991bb724e5e4c3771134b60dd
pc <jrguyyoo@hanmail.net>
2017-05-12 14:39:55 +0900

    SmartPhone

--- JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -5,20 +5,19 @@ package org.dimigo.inheritance;
  */
 public class PersonTest2 {
     private static void greeting(Person p){
+        System.out.println(p);
         p.sayHello();
         p.sayBye();
     }
     public static void main(String args[]){
-
         Person[] p=new Person[]{
                 new Person("Tom"),
                 new Korean("홍길동"),
                 new Japanese("다나카"),
                 new Chinese("왕밍"),
         };
-        for(int i=0;i<4;i++){
-            System.out.println(p[i]);
-            greeting(p[i]);
+        for(Person tmp : p){
+            greeting(tmp);
             System.out.println();
         }
     }
