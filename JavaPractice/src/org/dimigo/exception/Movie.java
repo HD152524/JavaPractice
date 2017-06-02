1a7326263abc052660e48d0a87adb5a043d080e5
pc <jrguyyoo@hanmail.net>
2017-06-02 14:28:18 +0900

    수정

--- JavaPractice/src/org/dimigo/exception/Movie.java
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -22,10 +22,7 @@ public class Movie {
 
     public void buyTicket(int age) throws Exception{
         if (age<limitAge){
-
             throw new Exception(title+"은/는 "+limitAge+"세 이상 관람가입니다.");
-
-
         }
         else {
             System.out.println(getTitle()+" 잘 보세요");
